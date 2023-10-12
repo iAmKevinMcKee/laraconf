249b9469692f272d3ef8d711a128162635c7e894
Kevin McKee <mckee.kevin@gmail.com>
2023-10-11 22:42:26 -0500

    wip

--- app/Filament/Resources/ConferenceResource.php
+++ app/Filament/Resources/ConferenceResource.php
@@ -25,50 +25,7 @@ class ConferenceResource extends Resource
     public static function form(Form $form): Form
     {
         return $form
-            ->schema([
-                Forms\Components\TextInput::make('name')
-                    ->label('Conference Name')
-                    ->default('My Conference')
-                    ->helperText('The name of the conference.')
-                    ->required()
-                    ->maxLength(60),
-                Forms\Components\MarkdownEditor::make('description')
-                    ->helperText('Hello')
-                    ->required(),
-                Forms\Components\DatePicker::make('start_date')
-                    ->native(false)
-                    ->required(),
-                Forms\Components\DateTimePicker::make('end_date')
-                    ->native(false)
-                    ->required(),
-                Forms\Components\Checkbox::make('is_published')
-                    ->default(true),
-                Forms\Components\Select::make('status')
-                    ->options([
-                        'draft' => 'Draft',
-                        'published' => 'Published',
-                        'archived' => 'Archived',
-                    ])
-                    ->required(),
-                Forms\Components\Select::make('region')
-                    ->live()
-                    ->enum(Region::class)
-                    ->options(Region::class),
-                Forms\Components\Select::make('venue_id')
-                    ->searchable()
-                    ->preload()
-                    ->createOptionForm(Venue::getForm())
-                    ->editOptionForm(Venue::getForm())
-                    ->relationship('venue', 'name', modifyQueryUsing: function (Builder $query, Forms\Get $get) {
-                        return $query->where('region', $get('region'));
-                    }),
-                Forms\Components\CheckboxList::make('speakers')
-                    ->relationship('speakers', 'name')
-                    ->options(
-                        Speaker::all()->pluck('name', 'id')
-                    )
-                    ->required(),
-            ]);
+            ->schema(Conference::getForm());
     }
 
     public static function table(Table $table): Table
--- app/Models/Conference.php
+++ app/Models/Conference.php
@@ -3,6 +3,17 @@
 namespace App\Models;
 
 use App\Enums\Region;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\Fieldset;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Get;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -33,5 +44,63 @@ class Conference extends Model
     public function talks(): BelongsToMany
     {
         return $this->belongsToMany(Talk::class);
+    }
+
+    public static function getForm(): array
+    {
+        return [
+            Section::make('Conference Details')
+                ->collapsible()
+                ->description('Provide some basic information about the conference.')
+                ->icon('heroicon-o-information-circle')
+                ->columns(2)
+            ->schema([
+                TextInput::make('name')
+                    ->columnSpanFull()
+                    ->label('Conference Name')
+                    ->default('My Conference')
+                    ->required()
+                    ->maxLength(60),
+                MarkdownEditor::make('description')
+                    ->columnSpanFull()
+                    ->required(),
+                DateTimePicker::make('start_date')
+                    ->native(false)
+                    ->required(),
+                DateTimePicker::make('end_date')
+                    ->native(false)
+                    ->required(),
+                Fieldset::make('Status')
+                    ->columns(1)
+                    ->schema([
+                        Select::make('status')
+                            ->options([
+                                'draft' => 'Draft',
+                                'published' => 'Published',
+                                'archived' => 'Archived',
+                            ])
+                            ->required(),
+                        Toggle::make('is_published')
+                            ->default(true),
+                    ]),
+            ]),
+
+            Section::make('Location')
+                ->columns(2)
+            ->schema([
+                Select::make('region')
+                    ->live()
+                    ->enum(Region::class)
+                    ->options(Region::class),
+                Select::make('venue_id')
+                    ->searchable()
+                    ->preload()
+                    ->createOptionForm(Venue::getForm())
+                    ->editOptionForm(Venue::getForm())
+                    ->relationship('venue', 'name', modifyQueryUsing: function (Builder $query, Get $get) {
+                        return $query->where('region', $get('region'));
+                    }),
+            ]),
+        ];
     }
 }
